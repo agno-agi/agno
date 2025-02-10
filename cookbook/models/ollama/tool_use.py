a2152ab17712e3b0c2c58124df9792c3681de3a1
Dirk Brand <dirkbrnd@gmail.com>
2025-02-10 13:49:22 +0200

    Update

--- cookbook/models/ollama/tool_use.py
+++ cookbook/models/ollama/tool_use.py
@@ -5,7 +5,7 @@ from agno.models.ollama import Ollama
 from agno.tools.duckduckgo import DuckDuckGoTools
 
 agent = Agent(
-    model=Ollama(id="hermes3"),
+    model=Ollama(id="llama3.1:8b"),
     tools=[DuckDuckGoTools()],
     show_tool_calls=True,
     markdown=True,
