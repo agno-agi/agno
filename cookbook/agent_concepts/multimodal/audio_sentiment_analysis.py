e0ea2c8810aaf74b0f86781d297b44d042615e4d
ansub <khanansub3@gmail.com>
2025-02-12 16:33:46 +0400

    fix formatting

--- cookbook/agent_concepts/multimodal/audio_sentiment_analysis.py
+++ cookbook/agent_concepts/multimodal/audio_sentiment_analysis.py
@@ -4,8 +4,8 @@ from agno.media import Audio
 from agno.models.google import Gemini
 
 agent = Agent(
-  model=Gemini(id="gemini-2.0-flash-exp"),
-  markdown=True,
+    model=Gemini(id="gemini-2.0-flash-exp"),
+    markdown=True,
 )
 
 url = "https://storage.googleapis.com/github-repo/generative-ai/gemini/use-cases/multimodal-sentiment-analysis/sample_conversation.wav"
@@ -16,7 +16,7 @@ audio_content = response.content
 # Give a sentiment analysis of this audio conversation. Use speaker A, speaker B to identify speakers.
 
 agent.print_response(
-  "Give a sentiment analysis of this audio conversation. Use speaker A, speaker B to identify speakers.",
-  audio=[Audio(content=audio_content)],
-  stream=True,
+    "Give a sentiment analysis of this audio conversation. Use speaker A, speaker B to identify speakers.",
+    audio=[Audio(content=audio_content)],
+    stream=True,
 )
