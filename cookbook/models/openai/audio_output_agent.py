e97f6cbaf4a5bc2c81014b08c8e1c1d692fdbbe6
anuragts <anuragsharma011011@gmail.com>
2025-02-18 18:31:13 +0530

    fix: remove audio only agent

--- cookbook/models/openai/audio_output_agent.py
+++ cookbook/models/openai/audio_output_agent.py
@@ -26,5 +26,6 @@ agent.print_response("What would be in a sequal of this story?")
 # Save the response audio to a file
 if agent.run_response.response_audio is not None:
     write_audio_to_file(
-        audio=agent.run_response.response_audio.content, filename="tmp/scary_story_sequal.wav"
+        audio=agent.run_response.response_audio.content,
+        filename="tmp/scary_story_sequal.wav",
     )
--- cookbook/playground/audio_conversation_agent.py
+++ cookbook/playground/audio_conversation_agent.py
@@ -19,22 +19,7 @@ audio_and_text_agent = Agent(
     # storage=SqliteAgentStorage(table_name="audio_agent", db_file="tmp/audio_agent.db"),
 )
 
-audio_only_agent = Agent(
-    agent_id="audio-only-agent",
-    name="Audio Only Chat Agent",
-    model=OpenAIChat(
-        id="gpt-4o-audio-preview",
-        modalities=["audio"],
-        audio={"voice": "alloy", "format": "pcm16"},  # Wav not supported for streaming
-    ),
-    debug_mode=True,
-    add_history_to_messages=True,
-    add_datetime_to_instructions=True,
-    storage=PostgresAgentStorage(table_name="agent_sessions", db_url=db_url),
-)
-
-
-app = Playground(agents=[audio_and_text_agent, audio_only_agent]).get_app()
+app = Playground(agents=[audio_and_text_agent]).get_app()
 
 if __name__ == "__main__":
     serve_playground_app("audio_conversation_agent:app", reload=True)
