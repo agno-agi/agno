1c204699a25834b41902a781e7998fc44e87dcbf
Shenghui <hsh-me@outlook.com>
2025-03-05 23:20:54 +0800

    add example for reasoning model using custom base_url

---
+++ cookbook/agent_concepts/reasoning/deepseek/custom_base_url.py
@@ -0,0 +1,22 @@
+import os
+
+## or set api key in bash shell
+os.environ["ALIYUN_API_KEY"] = "sk-**"
+
+## use Aliyun api for example, should comply with the OpenAI interface specification
+base_url = "https://dashscope.aliyuncs.com/compatible-mode/v1"
+
+task = "How many 'r' are in the word 'strawberry'?"
+
+reasoning_agent = Agent(
+    model=DeepSeek(
+        id="deepseek-v3", 
+        base_url=base_url,
+        api_key=getenv("ALIYUN_API_KEY")),
+    reasoning_model=DeepSeek(
+        id="deepseek-r1", 
+        base_url=base_url,
+        api_key=getenv("ALIYUN_API_KEY")),
+    markdown=True,
+)
+reasoning_agent.print_response(task, stream=True)
