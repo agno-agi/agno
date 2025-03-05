8715e201ad51cd93a45cd38bc8a3c142a0b66cff
Shenghui <hsh-me@outlook.com>
2025-03-06 00:20:33 +0800

    update  unofficial  deepseek reasoning model support within  areason function

--- libs/agno/agno/agent/agent.py
+++ libs/agno/agno/agent/agent.py
@@ -3024,8 +3024,10 @@ class Agent:
 
         # If a reasoning model is provided, use it to generate reasoning
         if reasoning_model_provided:
-            # Use DeepSeek for reasoning
-            if reasoning_model.__class__.__name__ == "DeepSeek" and reasoning_model.id == "deepseek-reasoner":
+            # Use deepseek reasoning model for reasoning, including unofficial deepseek reasoning model providers when setting base_url
+            # Use isinstance to check all model classes that inherit from OpenAIChat, including OpenAILike, DeepSeek, and others
+            if  isinstance(reasoning_model, OpenAIChat) and \
+                    ("deepseek-reasoner" in reasoning_model.id.lower() or "deepseek-r1" in reasoning_model.id.lower()):
                 from agno.reasoning.deepseek import aget_deepseek_reasoning, get_deepseek_reasoning_agent
 
                 ds_reasoning_agent = self.reasoning_agent or get_deepseek_reasoning_agent(
