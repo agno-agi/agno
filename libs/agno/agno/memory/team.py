980a85fb0b6bd16c6317b64b269168e454e3612a
anuragts <anuragsharma011011@gmail.com>
2025-03-06 18:51:16 +0530

    feat: add configurable system message update flag for TeamMemory

--- libs/agno/agno/memory/team.py
+++ libs/agno/agno/memory/team.py
@@ -50,6 +50,8 @@ class TeamMemory:
 
     # True when memory is being updated
     updating_memory: bool = False
+    # If True, update the system message when it changes
+    update_system_message_on_change: bool = True
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
