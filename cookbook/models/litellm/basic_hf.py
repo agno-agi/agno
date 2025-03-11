fe99b4ffa49c97a01581cc7a0820d0d41a719f1f
kausmeows <shuklakaustubh84@gmail.com>
2025-03-11 20:43:34 +0530

    update class name in example

--- cookbook/models/litellm/basic_hf.py
+++ cookbook/models/litellm/basic_hf.py
@@ -1,8 +1,8 @@
 from agno.agent import Agent
-from agno.models.litellm import LiteLLMSDK
+from agno.models.litellm import LiteLLM
 
 openai_agent = Agent(
-    model=LiteLLMSDK(
+    model=LiteLLM(
         id="huggingface/mistralai/Mistral-7B-Instruct-v0.2",
         top_p=0.95,
     ),
