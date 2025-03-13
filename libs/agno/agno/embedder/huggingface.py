c3c7596cebd2e39889e6c3a6c1749dc64e80167e
Tarun Jain <jaintarun.abd17@gmail.com>
2025-03-13 23:28:29 +0530

    stable huggingface embedding model for RAG

--- libs/agno/agno/embedder/huggingface.py
+++ libs/agno/agno/embedder/huggingface.py
@@ -17,7 +17,7 @@ except ImportError:
 class HuggingfaceCustomEmbedder(Embedder):
     """Huggingface Custom Embedder"""
 
-    id: str = "jinaai/jina-embeddings-v2-base-code"
+    id: str = "thenlper/gte-large"
     api_key: Optional[str] = getenv("HUGGINGFACE_API_KEY")
     client_params: Optional[Dict[str, Any]] = None
     huggingface_client: Optional[InferenceClient] = None
