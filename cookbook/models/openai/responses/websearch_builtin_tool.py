be084e97ed375934ae225c04cb3e6a088e981304
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-03-13 15:54:18 +0000

    Responses update

--- cookbook/models/openai/responses/websearch_builtin_tool.py
+++ cookbook/models/openai/responses/websearch_builtin_tool.py
@@ -1,10 +1,9 @@
-"""Run `pip install duckduckgo-search` to install dependencies."""
-
 from agno.agent import Agent
 from agno.models.openai import OpenAIResponses
 
 agent = Agent(
-    model=OpenAIResponses(id="gpt-4o", web_search=True),
+    model=OpenAIResponses(id="gpt-4o"),
+    tools=[{"type": "web_search_preview"}],
     markdown=True,
 )
 agent.print_response("Whats happening in France?")
