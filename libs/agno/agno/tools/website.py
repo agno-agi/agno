020d12d2bef5daa82a5073339ceada859bdc3028
Dirk Brand <dirkbrnd@gmail.com>
2025-03-19 13:25:16 +0200

    WIP

--- libs/agno/agno/tools/website.py
+++ libs/agno/agno/tools/website.py
@@ -2,15 +2,16 @@ import json
 from typing import List, Optional
 
 from agno.document import Document
+from agno.knowledge.combined import CombinedKnowledgeBase
 from agno.knowledge.website import WebsiteKnowledgeBase
 from agno.tools import Toolkit
 from agno.utils.log import logger
 
 
 class WebsiteTools(Toolkit):
-    def __init__(self, knowledge_base: Optional[WebsiteKnowledgeBase] = None):
+    def __init__(self, knowledge_base: Optional[WebsiteKnowledgeBase | CombinedKnowledgeBase] = None):
         super().__init__(name="website_tools")
-        self.knowledge_base: Optional[WebsiteKnowledgeBase] = knowledge_base
+        self.knowledge_base: Optional[WebsiteKnowledgeBase | CombinedKnowledgeBase] = knowledge_base
 
         if self.knowledge_base is not None and isinstance(self.knowledge_base, WebsiteKnowledgeBase):
             self.register(self.add_website_to_knowledge_base)
@@ -28,6 +29,27 @@ class WebsiteTools(Toolkit):
         """
         if self.knowledge_base is None:
             return "Knowledge base not provided"
+
+        logger.debug(f"Adding to knowledge base: {url}")
+        self.knowledge_base.urls.append(url)
+        logger.debug("Loading knowledge base.")
+        self.knowledge_base.load(recreate=False)
+        return "Success"
+
+    def add_website_to_combined_knowledge_base(self, url: str) -> str:
+        """This function adds a websites content to the knowledge base.
+        NOTE: The website must start with https:// and should be a valid website.
+
+        USE THIS FUNCTION TO GET INFORMATION ABOUT PRODUCTS FROM THE INTERNET.
+
+        :param url: The url of the website to add.
+        :return: 'Success' if the website was added to the knowledge base.
+        """
+        if self.knowledge_base is None:
+            return "Knowledge base not provided"
+
+        website_knowledge_base = None
+
 
         logger.debug(f"Adding to knowledge base: {url}")
         self.knowledge_base.urls.append(url)
