13c4e04d72bcbfecc8d8ab6680c87d6a3db0aac8
Dirk Brand <dirkbrnd@gmail.com>
2025-03-19 13:43:59 +0200

    UpdatE

--- cookbook/tools/website_tools.py
+++ cookbook/tools/website_tools.py
@@ -1,7 +1,17 @@
 from agno.agent import Agent
+from pathlib import Path
+
+from agno.knowledge.combined import CombinedKnowledgeBase
+from agno.knowledge.csv import CSVKnowledgeBase
+from agno.knowledge.pdf import PDFKnowledgeBase
+from agno.knowledge.pdf_url import PDFUrlKnowledgeBase
+from agno.knowledge.website import WebsiteKnowledgeBase
 from agno.tools.website import WebsiteTools
+from agno.vectordb.pgvector import PgVector
+
 
 agent = Agent(tools=[WebsiteTools()], show_tool_calls=True)
+
 agent.print_response(
     "Search web page: 'https://docs.agno.com/introduction'", markdown=True
 )
