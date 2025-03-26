7952449dfe3eef63e361e54e2f42ca8646699b32
chirag gupta <103719146+chiruu12@users.noreply.github.com>
2025-03-26 23:27:48 +0530

    Adding expand url tool

--- cookbook/tools/crawl4ai_tools.py
+++ cookbook/tools/crawl4ai_tools.py
@@ -1,5 +1,5 @@
 from agno.agent import Agent
 from agno.tools.crawl4ai import Crawl4aiTools
-
-agent = Agent(tools=[Crawl4aiTools(max_length=None,expand_url=True)], show_tool_calls=True)
+from agno.tools.webtools import WebTools
+agent = Agent(tools=[Crawl4aiTools(max_length=None),WebTools], show_tool_calls=True)
 agent.print_response("Tell me about https://tinyurl.com/k2fkfxra.")
