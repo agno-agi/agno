d13ac81f26fd32a37f1e0908174ad660fada94ea
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-04-07 23:59:17 +0100

    Fix: structured outputs

--- libs/agno/agno/reasoning/default.py
+++ libs/agno/agno/reasoning/default.py
@@ -75,8 +75,7 @@ def get_default_reasoning_agent(
             - **Insightful**: Offer innovative and unique perspectives where applicable.
         - Always explicitly handle errors and mistakes by resetting or revising steps immediately.
         - Adhere strictly to a minimum of {min_steps} and maximum of {max_steps} steps to ensure effective task resolution.
-        - Execute necessary tools proactively and without hesitation, clearly documenting tool usage.
-        - Only create a single instance of ReasoningSteps for your response.\
+        - Execute necessary tools proactively and without hesitation, clearly documenting tool usage.\
         """),
         tools=tools,
         show_tool_calls=False,
