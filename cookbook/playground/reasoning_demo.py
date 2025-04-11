19fab8f68dad299e8d0f61943b376ef7dc21679e
anuragts <anuragsharma011011@gmail.com>
2025-04-11 15:30:16 +0530

    fix: improvements

--- cookbook/playground/reasoning_demo.py
+++ cookbook/playground/reasoning_demo.py
@@ -1,8 +1,5 @@
 """Run `pip install openai exa_py duckduckgo-search yfinance pypdf sqlalchemy 'fastapi[standard]' youtube-transcript-api python-docx agno` to install dependencies."""
 
-from datetime import datetime
-from textwrap import dedent
-
 from agno.agent import Agent
 from agno.models.anthropic import Claude
 from agno.models.openai import OpenAIChat
@@ -22,24 +19,26 @@ cot_agent = Agent(
     agent_id="cot-agent",
     model=OpenAIChat(id="gpt-4o-mini"),
     storage=SqliteStorage(
-        table_name="simple_agent", db_file=agent_storage_file, auto_upgrade_schema=True
+        table_name="cot_agent", db_file=agent_storage_file, auto_upgrade_schema=True
     ),
     add_history_to_messages=True,
     num_history_responses=3,
     add_datetime_to_instructions=True,
     markdown=True,
-    reasoning=True,
+    reasoning=True, 
 )
 
 reasoning_model_agent = Agent(
     name="Reasoning Model Agent",
     role="Reasoning about Math",
+    agent_id="reasoning-model-agent",
     model=OpenAIChat(id="gpt-4o"),
     reasoning_model=OpenAIChat(id="o3-mini"),
     instructions=["You are a reasoning agent that can reason about math."],
     show_tool_calls=True,
     markdown=True,
     debug_mode=True,
+    storage=SqliteStorage(table_name="reasoning_model_agent", db_file=agent_storage_file, auto_upgrade_schema=True),
 )
 
 reasoning_tool_agent = Agent(
@@ -48,7 +47,7 @@ reasoning_tool_agent = Agent(
     agent_id="reasoning-tool-agent",
     model=OpenAIChat(id="gpt-4o-mini"),
     storage=SqliteStorage(
-        table_name="simple_agent", db_file=agent_storage_file, auto_upgrade_schema=True
+        table_name="reasoning_tool_agent", db_file=agent_storage_file, auto_upgrade_schema=True
     ),
     add_history_to_messages=True,
     num_history_responses=3,
@@ -60,6 +59,7 @@ reasoning_tool_agent = Agent(
 native_model_agent = Agent(
     model=OpenAIChat(id="o3-mini", reasoning_effort="high"),
     name="Native Model Agent",
+    agent_id="native_model_agent",
     tools=[
         YFinanceTools(
             stock_price=True,
@@ -71,18 +71,20 @@ native_model_agent = Agent(
     instructions="Use tables to display data.",
     show_tool_calls=True,
     markdown=True,
+    storage=SqliteStorage(table_name="native_model_agent", db_file=agent_storage_file, auto_upgrade_schema=True),
 )
 
 claude_thinking_agent = Agent(
     name="Claude Thinking Agent",
+    agent_id="claude_thinking_agent",
     model=Claude(
         id="claude-3-7-sonnet-20250219",
         max_tokens=2048,
         thinking={"type": "enabled", "budget_tokens": 1024},
     ),
     markdown=True,
+    storage=SqliteStorage(table_name="claude_thinking_agent", db_file=agent_storage_file, auto_upgrade_schema=True),
 )
-
 
 app = Playground(
     agents=[
