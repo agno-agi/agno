89f894641d7912462d380e768603ba2478275b0a
brikerman <eliyar917@gmail.com>
2025-04-11 14:44:14 +0800

    feat(litellm): add LiteLLMLangfuse class for enhanced observation

--- libs/agno/agno/models/litellm/__init__.py
+++ libs/agno/agno/models/litellm/__init__.py
@@ -1,2 +1,3 @@
 from agno.models.litellm.chat import LiteLLM
 from agno.models.litellm.litellm_openai import LiteLLMOpenAI
+from agno.models.litellm.litellm_langfuse import LiteLLMLangfuse
---
+++ libs/agno/agno/models/litellm/litellm_langfuse.py
@@ -0,0 +1,23 @@
+from agno.models.litellm.chat import LiteLLM
+from typing import Any, Dict
+
+
+try:
+    from langfuse.decorators import langfuse_context
+except ImportError:
+    raise ImportError(
+        "`langfuse` not installed. Please install it via `pip install langfuse`"
+    )
+
+
+class LiteLLMLangfuse(LiteLLM):
+
+    @property
+    def request_kwargs(self) -> Dict[str, Any]:
+        """Get the request kwargs for the LiteLLM API."""
+        completion_kwargs = super().request_kwargs
+        existed_trace_id = langfuse_context.get_current_trace_id()
+        if existed_trace_id:
+            completion_kwargs["metadata"] = completion_kwargs.get("metadata", {})
+            completion_kwargs["metadata"]["existing_trace_id"] = existed_trace_id
+        return completion_kwargs
