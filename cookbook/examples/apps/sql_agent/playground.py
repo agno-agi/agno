909c5adc4b1aea8002490575de25ace61bf1af52
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-04-20 16:58:13 -0700

    README

---
+++ cookbook/examples/apps/sql_agent/playground.py
@@ -0,0 +1,13 @@
+from agno.playground import Playground, serve_playground_app
+
+from agents import get_sql_agent
+
+sql_agent = get_sql_agent(name="SQL Agent", model_id="openai:o4-mini")
+reasoning_sql_agent = get_sql_agent(
+    name="Reasoning SQL Agent", model_id="anthropic:claude-3-7-sonnet-latest"
+)
+
+app = Playground(agents=[sql_agent, reasoning_sql_agent]).get_app()
+
+if __name__ == "__main__":
+    serve_playground_app("playground:app", reload=True)
