55a9f019daa475967c525d73912d200ef7f55dfd
Ashpreet <ashpreetbedi@users.noreply.github.com>
2025-04-20 23:16:20 -0700

    README

--- cookbook/examples/apps/sql_agent/playground.py
+++ cookbook/examples/apps/sql_agent/playground.py
@@ -1,6 +1,5 @@
-from agno.playground import Playground, serve_playground_app
-
 from agents import get_sql_agent
+from agno.playground import Playground, serve_playground_app
 
 sql_agent = get_sql_agent(name="SQL Agent", model_id="openai:o4-mini")
 reasoning_sql_agent = get_sql_agent(
