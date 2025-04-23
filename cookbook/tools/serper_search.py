b32a6eb3a5757372bc92bc14f4e02df1ea37921d
anuragts <anuragsharma011011@gmail.com>
2025-04-23 14:45:08 +0530

    fix: update

--- cookbook/tools/serper_search.py
+++ cookbook/tools/serper_search.py
@@ -1,5 +1,5 @@
 from agno.agent import Agent
 from agno.tools.serperapi import SerperApiTools
 
-agent = Agent(tools=[SerperApiTools(api_key="",gl="us")], show_tool_calls=True)
+agent = Agent(tools=[SerperApiTools(gl="us")], show_tool_calls=True)
 agent.print_response("Whats happening in the USA?", markdown=True)
