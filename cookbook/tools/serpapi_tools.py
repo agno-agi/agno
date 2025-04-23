41a255c39c11c8ba3f26eaabdffe5b012ddc249c
anuragts <anuragsharma011011@gmail.com>
2025-04-23 14:47:40 +0530

    fix: update

--- cookbook/tools/serpapi_tools.py
+++ cookbook/tools/serpapi_tools.py
@@ -1,5 +1,12 @@
 from agno.agent import Agent
 from agno.tools.serpapi import SerpApiTools
 
-agent = Agent(tools=[SerpApiTools()], show_tool_calls=True)
+agent = Agent(
+    name="SerpApiTools",
+    model="gpt-4o",
+    description="You are a helpful assistant that can search the web for information.",
+    tools=[SerpApiTools()],
+    show_tool_calls=True,
+)
+
 agent.print_response("Whats happening in the USA?", markdown=True)
