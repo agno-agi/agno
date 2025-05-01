4c3798f431dae54d746eaa65e1e8e3f107bd4207
Mustafa-Esoofally <coolmusta@gmail.com>
2025-05-01 13:15:09 -0400

    Handle file uploads for openAIChat

---
+++ cookbook/models/openai/chat/pdf_input_url.py
@@ -0,0 +1,16 @@
+from agno.agent import Agent
+from agno.media import File
+from agno.models.openai import OpenAIChat
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4o"),
+    markdown=True,
+    add_history_to_messages=True,
+)
+
+agent.print_response(
+    "Use the attached file to answer questions. Only refer to the file",
+    files=[File(url="https://agno-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf")],
+)
+
+agent.print_response("Suggest me a recipe from the attached file.")
