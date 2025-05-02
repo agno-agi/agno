ba5c366d9dab0af245f2bc76f05ae3da86094e97
Mustafa-Esoofally <coolmusta@gmail.com>
2025-05-02 14:38:03 -0400

    formatting

--- cookbook/examples/agents/recipe_rag_image.py
+++ cookbook/examples/agents/recipe_rag_image.py
@@ -1,22 +1,24 @@
-from typing import Optional
-import os
+"""Example: Multi-Modal RAG & Image Agent
+
+An agent that uses Llama 4 for multi-modal RAG and OpenAITools to create a visual, step-by-step image manual for a recipe.
+
+Run: `pip install openai agno groq cohere` to install the dependencies
+"""
+
+from pathlib import Path
 
 from agno.agent import Agent
-from agno.models.groq import Groq
 from agno.embedder.cohere import CohereEmbedder
-from agno.storage.agent.postgres import PostgresAgentStorage
-from agno.vectordb.pgvector import PgVector
-from agno.tools.openai import OpenAITools
 from agno.knowledge.pdf_url import PDFUrlKnowledgeBase
+from agno.models.groq import Groq
+from agno.tools.openai import OpenAITools
 from agno.utils.media import download_image
-from pathlib import Path
-# Database URL for Postgres vectordb and agent storage
-DB_URL = os.getenv("RAG_DB_URL", "postgresql+psycopg://ai:ai@localhost:5532/ai")
+from agno.vectordb.pgvector import PgVector
 
 knowledge_base = PDFUrlKnowledgeBase(
     urls=["https://agno-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
     vector_db=PgVector(
-        db_url=DB_URL,
+        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
         table_name="embed_vision_documents",
         embedder=CohereEmbedder(
             id="embed-v4.0",
@@ -31,13 +33,6 @@ agent = Agent(
     model=Groq(id="meta-llama/llama-4-scout-17b-16e-instruct"),
     tools=[OpenAITools()],
     knowledge=knowledge_base,
-    storage=PostgresAgentStorage(
-        db_url=DB_URL,
-        table_name="embed_vision_rag_sessions"
-    ),
-    # show_tool_calls=True,
-    add_history_to_messages=True,
-    markdown=True,
     instructions=[
         "You are a specialized recipe assistant.",
         "When asked for a recipe:",
@@ -46,10 +41,14 @@ agent = Agent(
         "3. Use the `generate_image` tool to create a visual, step-by-step image manual for the recipe.",
         "4. Present the recipe text clearly and mention that you have generated an accompanying image manual. Add instructions while generating the image.",
     ],
+    markdown=True,
     debug_mode=True,
 )
 
-response = agent.run("What is the recipe for a Thai curry?",)
+agent.print_response(
+    "What is the recipe for a Thai curry?",
+)
 
+response = agent.run_response
 if response.images:
     download_image(response.images[0].url, Path("tmp/recipe_image.png"))
