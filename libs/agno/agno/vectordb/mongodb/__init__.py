8d8ee5f30465197467406c3244807d40853122ce
kausmeows <shuklakaustubh84@gmail.com>
2025-05-11 22:08:56 +0530

    add cosmos mongo vector db

--- libs/agno/agno/vectordb/mongodb/__init__.py
+++ libs/agno/agno/vectordb/mongodb/__init__.py
@@ -1,5 +1,4 @@
+from agno.vectordb.mongodb.cosmos_mongodb import CosmosMongoDb
 from agno.vectordb.mongodb.mongodb import MongoDb
 
-__all__ = [
-    "MongoDb",
-]
+__all__ = ["MongoDb", "CosmosMongoDb"]
