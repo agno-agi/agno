b41f1ddadac7755a8799687e38422b862d8709c8
Kevin Truong <kevin.truong.ds@gmail.com>
2025-05-22 05:43:48 +0700

    add handle cost field

--- libs/agno/agno/models/message.py
+++ libs/agno/agno/models/message.py
@@ -69,6 +69,7 @@ class MessageMetrics:
 
     time: Optional[float] = None
     time_to_first_token: Optional[float] = None
+    cost: Optional[float] = None
 
     timer: Optional[Timer] = None
 
@@ -150,6 +151,14 @@ class MessageMetrics:
 
         # Handle time_to_first_token (take the first non-None value)
         result.time_to_first_token = self.time_to_first_token or other.time_to_first_token
+
+        # Handle cost
+        if self.cost is not None and other.cost is not None:
+            result.cost = self.cost + other.cost
+        elif self.cost is not None:
+            result.cost = self.cost
+        elif other.cost is not None:
+            result.cost = other.cost
 
         return result
 
