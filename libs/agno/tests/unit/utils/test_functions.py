c70efec6bbc5e176d10dc40265b988526c9db549
Dirk Brand <dirkbrnd@gmail.com>
2025-05-26 22:27:47 +0200

    Update

--- libs/agno/tests/unit/utils/test_functions.py
+++ libs/agno/tests/unit/utils/test_functions.py
@@ -151,7 +151,7 @@ def test_get_function_call_no_sanitization(sample_functions):
 
 def test_get_function_call_no_sanitization_2(sample_functions):
     """Test function call without argument sanitization."""
-    arguments = '{"param1": "None", "param2": "True"}'
+    arguments = '{"param1": None, "param2": True}'
     
     result = get_function_call(
         name="no_sanitize_function",
