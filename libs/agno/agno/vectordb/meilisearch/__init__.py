9464e245cd401e9749fd8ced9ef4676b0595dab6
wangzhihui <wzhmsgs@gmail.com>
2025-05-26 10:48:10 +0800

    feat: Add support for meilisearch in vectordb
    feat: Add support for meilisearch in vectordb

---
+++ libs/agno/agno/vectordb/meilisearch/__init__.py
@@ -0,0 +1,8 @@
+from agno.vectordb.meilisearch.meilisearch import MeiliSearch, MeiliSearchOperationError
+from agno.vectordb.search import SearchType
+
+__all__ = [
+    "MeiliSearch",
+    "SearchType",
+    "MeiliSearchOperationError",
+]
