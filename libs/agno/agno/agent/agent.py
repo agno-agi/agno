c49800b7c81d8bf2da4248917bbe4f018e9a7653
Dirk Brand <dirkbrnd@gmail.com>
2025-05-28 09:52:59 +0200

    Update

--- libs/agno/agno/agent/agent.py
+++ libs/agno/agno/agent/agent.py
@@ -2206,6 +2206,11 @@ class Agent:
         6. Save session to storage
         7. Save output to file if save_response_to_file is set
         """
+        # Start the Run by yielding a RunContinued event
+        if stream_intermediate_steps:
+            yield self.create_run_response("Run continued", session_id=session_id, event=RunEvent.run_continued)
+
+
         # 1. Handle the updated tools
         async for event in self._ahandle_tool_call_updates_stream(
             run_response=run_response, run_messages=run_messages, session_id=session_id
@@ -2215,10 +2220,6 @@ class Agent:
         # Get the index of the last "user" message in messages_for_run
         # We track this, so we can add messages after this index to the RunResponse and Memory
         index_of_last_user_message = len(run_messages.messages)
-
-        # Start the Run by yielding a RunContinued event
-        if stream_intermediate_steps:
-            yield self.create_run_response("Run continued", session_id=session_id, event=RunEvent.run_continued)
 
         # 2. Process model response
         async for event in self._ahandle_model_response_stream(
