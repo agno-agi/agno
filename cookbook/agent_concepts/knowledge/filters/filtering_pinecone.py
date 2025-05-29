cd2587d1759b60ef3b29bcb83d1b3cfc8d1646c9
databandit <8215071+5739n4@users.noreply.github.com>
2025-05-29 10:17:09 -0400

    fix-enable-filters-for-pinecone (#3361)
    ## Summary
    
    PineconeDB method search had filters commented out.
    
    ## Type of change
    
    - [X] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Improvement
    - [ ] Model update
    - [ ] Other:
    
    ---
    
    ## Checklist
    
    - [X] Code complies with style guidelines
    - [X] Ran format/validation scripts (`./scripts/format.sh` and
    `./scripts/validate.sh`)
    - [X] Self-review completed
    - [ ] Documentation updated (comments, docstrings)
    - [ ] Examples and guides: Relevant cookbook examples have been included
    or updated (if applicable)
    - [X] Tested in clean environment
    - [X] Tests added/updated (if applicable)
    
    ---
    
    ## Additional Notes
    
    none
    
    ---------
    
    Co-authored-by: Kaustubh <shuklakaustubh84@gmail.com>

---
+++ cookbook/agent_concepts/knowledge/filters/filtering_pinecone.py
@@ -0,0 +1,38 @@
+from agno.agent import Agent
+from agno.knowledge.website import WebsiteKnowledgeBase
+from agno.models.openai import OpenAIChat
+from agno.vectordb.pineconedb import PineconeDb
+
+# Initialize Pinecone
+vector_db = PineconeDb(
+    dimension=1536,
+    name="agnodocs",
+    spec=dict(serverless=dict(cloud="aws", region="us-east-1")),
+)
+
+# Step 1: Initialize knowledge base with documents and metadata
+knowledge_base = WebsiteKnowledgeBase(
+    num_documents=3,
+    urls=["https://docs.agno.com/introduction"],
+    max_depth=5,
+    max_links=10,
+    vector_db=vector_db,
+)
+
+# Load all documents into the vector database
+knowledge_base.load(recreate=True)
+
+# Step 2: Query the knowledge base with different filter combinations
+# ------------------------------------------------------------------------------
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4.1-nano", temperature=0),
+    knowledge=knowledge_base,
+    search_knowledge=True,
+)
+
+agent.print_response(
+    "What integrations are shown?",
+    knowledge_filters={"url": "https://docs.agno.com/examples/introduction"},
+    markdown=True,
+)
