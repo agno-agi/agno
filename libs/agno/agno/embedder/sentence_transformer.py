56d9f4ed0ffdaf107b5ded1b2c27c2224dc20b66
Kaustubh <shuklakaustubh84@gmail.com>
2025-06-03 09:33:32 +0530

    update

--- libs/agno/agno/embedder/sentence_transformer.py
+++ libs/agno/agno/embedder/sentence_transformer.py
@@ -2,11 +2,17 @@ import platform
 import numpy as np
 from dataclasses import dataclass
 from typing import Dict, List, Optional, Tuple, Union
-from sentence_transformers import SentenceTransformer
 
 from agno.embedder.base import Embedder
 from agno.utils.log import logger
 
+try:
+    from sentence_transformers import SentenceTransformer
+
+    if platform.system() == "Windows":
+        import numpy as np
+except ImportError:
+    raise ImportError("`sentence-transformers` not installed, please run `pip install sentence-transformers`")
 
 @dataclass
 class SentenceTransformerEmbedder(Embedder):
