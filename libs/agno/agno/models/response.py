d6cbb0057493db20c914d52335b02de0a133ec7d
danielna-lqc <danielna@liquidity-capital.com>
2025-06-04 17:18:49 +0400

    fix(memory): Use walrus operator to account for None

--- libs/agno/agno/models/response.py
+++ libs/agno/agno/models/response.py
@@ -69,9 +69,11 @@ class ToolExecution:
             confirmed=data.get("confirmed"),
             confirmation_note=data.get("confirmation_note"),
             requires_user_input=data.get("requires_user_input"),
-            user_input_schema=[UserInputField.from_dict(field) for field in data.get("user_input_schema")]
-            if data.get("user_input_schema") is not None
-            else [],
+            user_input_schema=(
+                [UserInputField.from_dict(field) for field in schema_data]
+                if (schema_data := data.get("user_input_schema")) is not None
+                else []
+            ),
             external_execution_required=data.get("external_execution_required"),
             metrics=MessageMetrics(**(data.get("metrics", {}) or {})),
         )
