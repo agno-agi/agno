f1cb90416d7266fe8f18d979a7a2a2e96dc12061
Shreyas <69378056+shreyaspranav@users.noreply.github.com>
2025-06-23 03:29:50 +0530

    fix: Add a check to create a directory if it doesn't exist while saving an audio file

--- libs/agno/agno/utils/audio.py
+++ libs/agno/agno/utils/audio.py
@@ -1,16 +1,21 @@
 import base64
+import os
 
 from agno.utils.log import log_info
 
 
-def write_audio_to_file(audio, filename: str):
+def write_audio_to_file(audio, filepath: str):
     """
     Write base64 encoded audio file to disk.
 
     :param audio: Base64 encoded audio file
-    :param filename: The filename to save the audio to
+    :param filepath: The filepath to save the audio to
     """
     wav_bytes = base64.b64decode(audio)
-    with open(filename, "wb") as f:
+
+    # Create `filepath` directory if it doesn't exist.
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+    with open(filepath, "wb") as f:
         f.write(wav_bytes)
-    log_info(f"Audio file saved to {filename}")
+    log_info(f"Audio file saved to {filepath}")
