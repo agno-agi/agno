2607d9557d237f28a01087310f8ccc5970bc17f0
Willem Carel de Jongh <willemcarel@gmail.com>
2025-06-25 14:09:09 +0200

    feat: Knowledge pagination

--- libs/agno/agno/document/document_v2.py
+++ libs/agno/agno/document/document_v2.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import List, Optional, Union
 
 from agno.document.reader import Reader
+from agno.knowledge.cloud_storage.cloud_storage import CloudStorageConfig
 
 
 @dataclass
@@ -12,11 +13,67 @@ class DocumentContent:
 
 @dataclass
 class DocumentV2:  # We will rename this to Document
-    name: str
     id: Optional[str] = None
+    name: Optional[str] = None
     description: Optional[str] = None
-    paths: Optional[Union[str, List[str]]] = None
-    urls: Optional[Union[str, List[str]]] = None
-    content: Optional[DocumentContent] = None
+    path: Optional[str] = None
+    url: Optional[str] = None
+    content: Optional[str] = None
     metadata: Optional[dict] = None
+    topics: Optional[List[str]] = None
+    config: Optional[CloudStorageConfig] = None
     reader: Optional[Reader] = None
+
+
+# readers: List[Reader] = [
+#         "PDFREADER": PDFReader(),
+#         "URLREADER": URLReader(),
+#         "WEBSITEREADER": WebsiteReader(),
+#         "S3READER": S3PDFReader(),
+#         "AZUREREADER": AzurePDFReader(),
+#     ]
+
+
+
+# "UUID": PDF: [
+#     "id1": PDFReader(),
+#     "id2": PDFURLReader(),
+# ]
+
+# URL: [
+#     URLReader(),
+#     WebsiteReader(),
+# ]
+
+# JSON: [
+#     JSONReader(),
+# ]
+
+
+
+
+
+
+# LOAD:
+
+# DocumentStore
+# DocumentV2   -> Paths URLS
+# Paths
+# URLs
+# Content
+
+
+
+# ADD DOCUMENT
+# DocumentV2   -> Paths URLS
+# Paths
+# URLs
+# Content
+
+
+
+# VECTOR DB
+# DocumentsDB
+# DocumentStore
+
+# Manager
