ddee91e377dbe2070824e5728935e8818105dac9
Juliette Sivan <91728573+juliette0704@users.noreply.github.com>
2025-07-21 15:15:12 +0200

    feat: add linkup search tool for real time web research

--- cookbook/tools/linkup_tools.py
+++ cookbook/tools/linkup_tools.py
@@ -2,4 +2,4 @@ from agno.agent import Agent
 from agno.tools.linkup import LinkupTools
 
 agent = Agent(tools=[LinkupTools()], show_tool_calls=True)
-agent.print_response("Search linkup for 'language models'", markdown=True)
+agent.print_response("Whats happening in France?", markdown=True)
