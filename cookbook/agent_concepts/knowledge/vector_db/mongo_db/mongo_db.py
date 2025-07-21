ae53f28fe40414f9b62eebe69de0b39b38dc6a52
kausmeows <shuklakaustubh84@gmail.com>
2025-07-21 21:20:19 +0530

    update

--- cookbook/agent_concepts/knowledge/vector_db/mongo_db/mongo_db.py
+++ cookbook/agent_concepts/knowledge/vector_db/mongo_db/mongo_db.py
@@ -8,16 +8,15 @@ from agno.vectordb.mongodb import MongoDb
 # Atlas: "mongodb+srv://<username>:<password>@cluster.mongodb.net/?retryWrites=true&w=majority"
 
 # Example with MongoDB Atlas (replace with your credentials)
-username = ""
-password = ""
+username = "kaus"
+password = "agno123"
 encoded_password = urllib.parse.quote_plus(password)
 connection_string = f"mongodb+srv://{username}:{encoded_password}@agno-mdb.xrv6kn1.mongodb.net/?retryWrites=true&w=majority&appName=agno-mdb"
 
 vector_db = MongoDb(
-    collection_name="vectors",
+    collection_name="recipes",
     db_url=connection_string,
-    database="agno",
-    search_index_name="agno_test",
+    search_index_name="recipes",
 )
 
 # Create Knowledge Instance with MongoDB
@@ -42,5 +41,5 @@ agent.print_response("List down the ingredients to make Massaman Gai", markdown=
 # vector_db.delete_by_metadata({"doc_type": "recipe_book"})
 
 # Check final count
-print(f"Documents remaining: {knowledge.vector_store.get_count()}")
+print(f"Documents remaining: {vector_db.get_count()}")
 
