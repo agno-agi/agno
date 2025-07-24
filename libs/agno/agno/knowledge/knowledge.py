54a789c7fd9b26273fc2e0db711eb389846b039d
Willem Carel de Jongh <willemcarel@gmail.com>
2025-07-24 06:21:18 +0200

    Enable async agent knowledge search

--- libs/agno/agno/knowledge/knowledge.py
+++ libs/agno/agno/knowledge/knowledge.py
@@ -509,6 +509,8 @@ class Knowledge:
         self, query: str, max_results: Optional[int] = None, filters: Optional[Dict[str, Any]] = None
     ) -> List[Document]:
         """Returns relevant documents matching a query"""
+        log_info(f"Sync Searching for documents with query: {query}")
+
         try:
             if self.vector_store is None:
                 log_warning("No vector db provided")
@@ -525,6 +527,8 @@ class Knowledge:
         self, query: str, max_results: Optional[int] = None, filters: Optional[Dict[str, Any]] = None
     ) -> List[Document]:
         """Returns relevant documents matching a query"""
+
+        log_info(f"Aysnc Searching for documents with query: {query}")
         try:
             if self.vector_store is None:
                 log_warning("No vector db provided")
