78f339fa0f15e0ea4326d81a77627b758e41aa12
Mustafa-Esoofally <coolmusta@gmail.com>
2025-08-06 03:50:34 +0530

    update file name

--- cookbook/agent_concepts/knowledge/pdf_kb_password.py
+++ cookbook/agent_concepts/knowledge/pdf_kb_password.py
@@ -6,12 +6,12 @@ db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
 # Create a knowledge base with the PDFs from the data/pdfs directory
 knowledge_base = PDFKnowledgeBase(
-    path="pw_protected.pdf",
+    path="tmp/ThaiRecipes_protected.pdf",
     vector_db=PgVector(
         table_name="pdf_documents_password",
         db_url=db_url,
     ),
-    reader=PDFReader(chunk=True, password="samplefiles"),
+    reader=PDFReader(chunk=True, password="ThaiRecipes"),
 )
 # Load the knowledge base
 knowledge_base.load(recreate=True)
@@ -23,4 +23,4 @@ agent = Agent(
     show_tool_calls=True,
 )
 
-agent.print_response("What are the different types of Encryption?")
+agent.print_response("Give me the recipe for pad thai")
