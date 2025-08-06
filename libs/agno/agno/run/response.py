8e4ee2e4da8daf9e2ea308e6b9b92bb2da7041eb
Mustafa-Esoofally <coolmusta@gmail.com>
2025-08-06 15:28:56 +0530

    Keep extra_data for RunResponse

--- libs/agno/agno/run/response.py
+++ libs/agno/agno/run/response.py
@@ -73,7 +73,7 @@ class RunResponseContentEvent(BaseAgentRunResponseEvent):
     citations: Optional[Citations] = None
     response_audio: Optional[AudioResponse] = None  # Model audio response
     image: Optional[ImageArtifact] = None  # Image attached to the response
-    metadata: Optional[RunResponseMetaData] = None
+    extra_data: Optional[RunResponseMetaData] = None
 
 
 @dataclass
