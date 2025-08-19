f5e4563ad86dcbacd215f97e01ecdb13f91bb907
Willem Carel de Jongh <willemcarel@gmail.com>
2025-08-19 12:55:13 +0200

    fix: GCS KB

--- cookbook/agent_concepts/knowledge/gcs_pdf_kb.py
+++ cookbook/agent_concepts/knowledge/gcs_pdf_kb.py
@@ -19,8 +19,8 @@ from agno.vectordb.pgvector import PgVector
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
 knowledge_base = GCSPDFKnowledgeBase(
-    bucket_name="thai-recepies",
-    blob_name="ThaiRecipes.pdf",
+    bucket_name="your-gcs-bucket",
+    blob_name="path/to/your.pdf",
     vector_db=PgVector(table_name="recipes", db_url=db_url),
 )
 knowledge_base.load(recreate=False)  # Comment out after first run
