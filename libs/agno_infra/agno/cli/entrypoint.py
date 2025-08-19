fa0ff7d803653e02eff0376fc29436ebe2993021
Dirk Brand <dirkbrnd@gmail.com>
2025-08-19 23:11:12 +0200

    Remove unused routes

--- libs/agno_infra/agno/cli/entrypoint.py
+++ libs/agno_infra/agno/cli/entrypoint.py
@@ -15,10 +15,9 @@ agno_cli = typer.Typer(
 Agno is a lightweight framework for building Agent Systems.
 \b
 Usage:
-1. Run `ag init` to create a new /infra directory
-2. Run `ag infra create` to create a new Agno infrastructure from template
-3. Run `ag infra up` to start the infrastructure
-4. Run `ag infra down` to stop the infrastructure
+1. Run `ag infra create` to create a new Agentics Infrastructure project from a template
+2. Run `ag infra up` to start the infrastructure
+3. Run `ag infra down` to stop the infrastructure
 """,
     no_args_is_help=True,
     add_completion=False,
@@ -27,80 +26,6 @@ Usage:
     subcommand_metavar="[COMMAND] [OPTIONS]",
     pretty_exceptions_show_locals=False,
 )
-
-
-@agno_cli.command(short_help="Initialize Agno, use -r to reset")
-def init(
-    reset: bool = typer.Option(False, "--reset", "-r", help="Reset Agno", show_default=True),
-    print_debug_log: bool = typer.Option(
-        False,
-        "-d",
-        "--debug",
-        help="Print debug logs.",
-    ),
-):
-    """
-    \b
-    Initialize Agno, use -r to reset
-
-    \b
-    Examples:
-    * `ag init`    -> Initializing Agno
-    * `ag init -r` -> Reset Agno
-    """
-    if print_debug_log:
-        set_log_level_to_debug()
-
-    from agno.cli.operator import initialize_agno_cli
-
-    initialize_agno_cli(reset=reset)
-
-
-@agno_cli.command(short_help="Reset Agno installation")
-def reset(
-    print_debug_log: bool = typer.Option(
-        False,
-        "-d",
-        "--debug",
-        help="Print debug logs.",
-    ),
-):
-    """
-    \b
-    Reset the existing Agno configuration
-    """
-    if print_debug_log:
-        set_log_level_to_debug()
-
-    from agno.cli.operator import initialize_agno_cli
-
-    initialize_agno_cli(reset=True)
-
-
-@agno_cli.command(short_help="Print Agno config")
-def config(
-    print_debug_log: bool = typer.Option(
-        False,
-        "-d",
-        "--debug",
-        help="Print debug logs.",
-    ),
-):
-    """Print your current Agno config"""
-    if print_debug_log:
-        set_log_level_to_debug()
-
-    from agno.cli.config import AgnoCliConfig
-    from agno.cli.console import log_config_not_available_msg
-    from agno.cli.operator import initialize_agno_cli
-
-    agno_config: Optional[AgnoCliConfig] = AgnoCliConfig.from_saved_config()
-    if not agno_config:
-        agno_config = initialize_agno_cli()
-        if not agno_config:
-            log_config_not_available_msg()
-            return
-    agno_config.print_to_cli(show_all=True)
 
 
 agno_cli.add_typer(infra_subcommands)
