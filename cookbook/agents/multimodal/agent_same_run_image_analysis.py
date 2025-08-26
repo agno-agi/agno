5e5a4576bc1fc92fccd3a11363881eef39a25ba3
kausmeows <shuklakaustubh84@gmail.com>
2025-08-26 17:18:47 +0530

    update

--- cookbook/agents/multimodal/agent_same_run_image_analysis.py
+++ cookbook/agents/multimodal/agent_same_run_image_analysis.py
@@ -4,9 +4,13 @@ from agno.tools.dalle import DalleTools
 # Create an Agent with the DALL-E tool
 agent = Agent(tools=[DalleTools()], name="DALL-E Image Generator")
 
-agent.print_response(
+response = agent.run(
     "Generate an image of a dog and tell what color the dog is.",
     markdown=True,
     debug_mode=True,
 )
+
+if response.images:
+    print('Agent Response', response.content)
+    print(response.images[0].url)
 
