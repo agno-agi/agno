0d1fabce1fc26fefcb6c11e89425722ab717886c
manuhortet <manuhortet@gmail.com>
2025-09-05 11:38:39 +0200

    update

--- cookbook/agent_os/mcp/mcp_tools_example.py
+++ cookbook/agent_os/mcp/mcp_tools_example.py
@@ -8,25 +8,13 @@ from agno.agent import Agent
 from agno.db.postgres import PostgresDb
 from agno.models.anthropic import Claude
 from agno.os import AgentOS
-from agno.tools.mcp import (  # noqa: F401
-    MCPTools,
-    MultiMCPTools,
-    StreamableHTTPClientParams,
-)
+from agno.tools.mcp import MCPTools
 
 # Setup the database
 db = PostgresDb(db_url="postgresql+psycopg://ai:ai@localhost:5532/ai")
 
-# server_params = [
-#     StreamableHTTPClientParams(url="https://docs-v2.agno.com/mcp"),
-#     StreamableHTTPClientParams(url="https://api.githubcopilot.com/mcp/"),
-# ]
-
-# mcp_tools = MultiMCPTools(server_params_list=server_params)
 mcp_tools = MCPTools(transport="streamable-http", url="https://docs-v2.agno.com/mcp")
-mcp_tools_2 = MCPTools(
-    transport="streamable-http", url="https://api.githubcopilot.com/mcp/"
-)
+
 
 # Setup basic agents, teams and workflows
 agno_support_agent = Agent(
@@ -34,10 +22,7 @@ agno_support_agent = Agent(
     name="Agno Support Agent",
     model=Claude(id="claude-sonnet-4-0"),
     db=db,
-    tools=[
-        mcp_tools,
-        mcp_tools_2,
-    ],
+    tools=[mcp_tools],
     add_history_to_context=True,
     num_history_runs=3,
     markdown=True,
