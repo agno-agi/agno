67503d439f512a7ed5ba6a4c578f811c9e32efc3
Willem Carel de Jongh <willemcarel@gmail.com>
2025-09-10 17:16:26 +0200

    feat: Support custom encoding on reader class

--- cookbook/knowledge/readers/csv_reader_custom_encodings.py
+++ cookbook/knowledge/readers/csv_reader_custom_encodings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 from agno.agent import Agent
 from agno.knowledge.knowledge import Knowledge
-from agno.vectordb.pgvector import PgVector
 from agno.knowledge.reader.csv_reader import CSVReader
 from agno.models.openai import OpenAIChat
+from agno.vectordb.pgvector import PgVector
 
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
@@ -27,7 +27,12 @@ agent = Agent(
 
 if __name__ == "__main__":
     # Comment out after first run
-    asyncio.run(knowledge.add_content_async(url="https://agno-public.s3.amazonaws.com/demo_data/IMDB-Movie-Data.csv", reader=CSVReader(encoding="gb2312")))
+    asyncio.run(
+        knowledge.add_content_async(
+            url="https://agno-public.s3.amazonaws.com/demo_data/IMDB-Movie-Data.csv",
+            reader=CSVReader(encoding="gb2312"),
+        )
+    )
 
     # Create and use the agent
     asyncio.run(agent.aprint_response("What is the csv file about", markdown=True))
