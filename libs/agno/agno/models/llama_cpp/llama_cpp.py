b0bfda6716b1bb073d503facf53c8551af769410
ysolanky <yash@phidata.com>
2025-09-18 10:34:24 -0400

    feat: llama cpp

--- libs/agno/agno/models/llama_cpp/llama_cpp.py
+++ libs/agno/agno/models/llama_cpp/llama_cpp.py
@@ -9,15 +9,15 @@ class LlamaCpp(OpenAILike):
     A class for interacting with Llama CPP.
 
     Attributes:
-        id (str): The id of the Llama CPP model. Default is "qwen2.5-7b-instruct-1m".
-        name (str): The name of this chat model instance. Default is "Llama CPP".
-        provider (str): The provider of the model. Default is "Llama CPP".
+        id (str): The id of the Llama CPP model. Default is "ggml-org/gpt-oss-20b-GGUF".
+        name (str): The name of this chat model instance. Default is "LlamaCPP".
+        provider (str): The provider of the model. Default is "LlamaCPP".
         base_url (str): The base url to which the requests are sent.
     """
 
-    id: str = "qwen2.5-7b-instruct-1m"
-    name: str = "Llama CPP"
-    provider: str = "Llama CPP"
+    id: str = "ggml-org/gpt-oss-20b-GGUF"
+    name: str = "LlamaCPP"
+    provider: str = "LlamaCPP"
 
     base_url: str = "http://127.0.0.1:8080/v1"
 
