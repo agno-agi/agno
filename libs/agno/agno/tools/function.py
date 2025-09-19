8ff67233e225bd46ce28761a19ca7255a4da99b0
Samuel Jupe <samueljacobjupe@gmail.com>
2025-09-19 16:38:12 +0100

    fix forward ref issue

--- libs/agno/agno/tools/function.py
+++ libs/agno/agno/tools/function.py
@@ -387,9 +387,14 @@ class Function(BaseModel):
             return func
         # Wrap the callable with validate_call
         else:
-            wrapped = validate_call(func, config=dict(arbitrary_types_allowed=True))  # type: ignore
-            wrapped._wrapped_for_validation = True  # Mark as wrapped to avoid infinite recursion
-            return wrapped
+            try:
+                wrapped = validate_call(func, config=dict(arbitrary_types_allowed=True))  # type: ignore
+                wrapped._wrapped_for_validation = True  # Mark as wrapped to avoid infinite recursion
+                return wrapped
+            except Exception:
+                # If validate_call fails (e.g., due to unresolved forward references),
+                # return the original function unwrapped
+                return func
 
     def process_schema_for_strict(self):
         self.parameters["additionalProperties"] = False
