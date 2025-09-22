9f1c644eec5e8c0102256f1e703bb2371bfbe4b7
Martin Schaer <martin.schaer@surrealdb.com>
2025-09-22 20:12:07 +0100

    clean

--- libs/agno/agno/db/surrealdb/queries.py
+++ libs/agno/agno/db/surrealdb/queries.py
@@ -1,19 +1,6 @@
 from textwrap import dedent
 from typing import Any, Final, Literal
 
-CREATE_TABLE_QUERY: Final[str] = dedent("""
-    DEFINE TABLE IF NOT EXISTS {table} SCHEMAFUL;
-
-    DEFINE FIELD IF NOT EXISTS memory ON {table} FLEXIBLE TYPE object;
-    DEFINE FIELD IF NOT EXISTS user ON {table} TYPE record<user>;
-    DEFINE FIELD IF NOT EXISTS time ON {table} TYPE object DEFAULT ALWAYS {{}};
-    DEFINE FIELD IF NOT EXISTS time.created_at ON {table} TYPE datetime VALUE time::now() READONLY;
-    DEFINE FIELD IF NOT EXISTS time.updated_at ON {table} TYPE datetime VALUE time::now() READONLY;
-
-    DEFINE INDEX IF NOT EXISTS idx_{table}_user ON {table} FIELDS user;
-""")
-
-
 OPERATOR = Literal["=", "!=", "<=", ">=", "~", "IN", "CONTAINSANY"]
 
 COUNT_QUERY: Final[str] = dedent("""
