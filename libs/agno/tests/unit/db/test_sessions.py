0ed47116056828cc520973bc8fee201a9804d140
Dirk Brand <dirkbrnd@gmail.com>
2025-09-22 11:52:12 +0200

    Update

---
+++ libs/agno/tests/unit/db/test_sessions.py
@@ -0,0 +1,14 @@
+from agno.session.team import TeamSession
+from agno.session.agent import AgentSession
+
+def test_agent_session_serialization_empty_runs():
+    sess1 = AgentSession(session_id="s3", runs=[])
+    dump = sess1.to_dict()
+    sess2 = AgentSession.from_dict(dump)
+    assert sess1 == sess2
+
+def test_team_session_serialization_empty_runs():
+    sess1 = TeamSession(session_id="s3", runs=[])
+    dump = sess1.to_dict()
+    sess2 = TeamSession.from_dict(dump)
+    assert sess1 == sess2
