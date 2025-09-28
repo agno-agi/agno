86fc073f16100d8fe7ff39d8085f55dd6f7946ed
Adi Berkowitz <ab@lev.co>
2025-09-28 15:05:57 -0400

    fix: Add parent run id to all events from team

--- libs/agno/agno/team/team.py
+++ libs/agno/agno/team/team.py
@@ -5464,6 +5464,7 @@ class Team:
                     check_if_run_cancelled(member_agent_run_output_event)
 
                     # Yield the member event directly
+                    member_agent_run_output_event.parent_run_id = run_response.run_id
                     yield member_agent_run_output_event
             else:
                 member_agent_run_response = member_agent.run(  # type: ignore
@@ -5589,6 +5590,7 @@ class Team:
                     check_if_run_cancelled(member_agent_run_response_event)
 
                     # Yield the member event directly
+                    member_agent_run_response_event.parent_run_id = run_response.run_id
                     yield member_agent_run_response_event
             else:
                 member_agent_run_response = await member_agent.arun(  # type: ignore
@@ -5704,6 +5706,7 @@ class Team:
                         check_if_run_cancelled(member_agent_run_response_chunk)
 
                         # Yield the member event directly
+                        member_agent_run_response_chunk.parent_run_id = run_response.run_id
                         yield member_agent_run_response_chunk
 
                 else:
@@ -5815,6 +5818,7 @@ class Team:
                                 member_agent_run_response = member_agent_run_output_event  # type: ignore
                                 break
                             check_if_run_cancelled(member_agent_run_output_event)
+                            member_agent_run_output_event.parent_run_id = run_response.run_id
                             await queue.put(member_agent_run_output_event)
                     finally:
                         _process_delegate_task_to_member(
