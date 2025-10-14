e7d276e028d666f93495430ef284f7ee7a7f9604
Mihail Vasilev <922814+mickvav@users.noreply.github.com>
2025-10-15 00:01:14 +0100

    new functiond for file tool

--- libs/agno/agno/tools/file.py
+++ libs/agno/agno/tools/file.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Tuple
 
 from agno.tools import Toolkit
 from agno.utils.log import log_debug, log_error, log_info
@@ -12,14 +12,25 @@ class FileTools(Toolkit):
         base_dir: Optional[Path] = None,
         enable_save_file: bool = True,
         enable_read_file: bool = True,
+        enable_delete_file: bool = False,
         enable_list_files: bool = True,
         enable_search_files: bool = True,
+        enable_read_file_chunk: bool = True,
+        enable_replace_file_chunk: bool = True,
+        expose_base_directory: bool = False,
+        max_file_length: int = 10000000,
+        max_file_lines: int = 100000,
+        line_separator: str = "\n",
         all: bool = False,
         **kwargs,
     ):
-        self.base_dir: Path = base_dir or Path.cwd()
+        self.base_dir: Path = base_dir.resolve() or Path.cwd()
 
         tools: List[Any] = []
+        self.max_file_length = max_file_length
+        self.max_file_lines = max_file_lines
+        self.line_separator = line_separator
+        self.expose_base_directory = expose_base_directory
         if all or enable_save_file:
             tools.append(self.save_file)
         if all or enable_read_file:
@@ -28,6 +39,12 @@ class FileTools(Toolkit):
             tools.append(self.list_files)
         if all or enable_search_files:
             tools.append(self.search_files)
+        if all or enable_delete_file:
+            tools.append(self.delete_file)
+        if all or enable_read_file_chunk:
+            tools.append(self.read_file_chunk)
+        if all or enable_replace_file_chunk:
+            tools.append(self.replace_file_chunk)
 
         super().__init__(name="file_tools", tools=tools, **kwargs)
 
@@ -40,7 +57,10 @@ class FileTools(Toolkit):
         :return: The file name if successful, otherwise returns an error message.
         """
         try:
-            file_path = self.base_dir.joinpath(file_name)
+            safe, file_path = self.check_escape(file_name)
+            if not (safe):
+                log_error(f"Attempted to save file: {file_name}")
+                return "Error saving file"
             log_debug(f"Saving contents to {file_path}")
             if not file_path.parent.exists():
                 file_path.parent.mkdir(parents=True, exist_ok=True)
@@ -53,6 +73,54 @@ class FileTools(Toolkit):
             log_error(f"Error saving to file: {e}")
             return f"Error saving to file: {e}"
 
+    def read_file_chunk(self, file_name: str, start_line: int, end_line: int) -> str:
+        """Reads the contents of the file `file_name` and returns lines from start_line to end_line.
+
+        :param file_name: The name of the file to read.
+        :param start_line: Number of first line in the returned chunk
+        :param end_line: Number of the last line in the returned chunk
+
+        :return: The contents of the selected chunk
+        """
+        try:
+            log_info(f"Reading file: {file_name}")
+            safe, file_path = self.check_escape(file_name)
+            if not (safe):
+                log_error(f"Attempted to read file: {file_name}")
+                return "Error reading file"
+            contents = file_path.read_text(encoding="utf-8")
+            lines = contents.split(self.line_separator)
+            return self.line_separator.join(lines[start_line : end_line + 1])
+        except Exception as e:
+            log_error(f"Error reading file: {e}")
+            return f"Error reading file: {e}"
+
+    def replace_file_chunk(self, file_name: str, start_line: int, end_line: int, chunk: str) -> str:
+        """Reads the contents of the file, replaces lines
+        between start_line and end_line with chunk and writes the file
+
+        :param file_name: The name of the file to process.
+        :param start_line: Number of first line in the replaced chunk
+        :param end_line: Number of the last line in the replaced chunk
+        :param chunk: String to be inserted instead of lines from start_line to end_line. Can have multiple lines.
+
+        :return: file name if successfull, error message otherwise
+        """
+        try:
+            log_info(f"Patching file: {file_name}")
+            safe, file_path = self.check_escape(file_name)
+            if not (safe):
+                log_error(f"Attempted to read file: {file_name}")
+                return "Error reading file"
+            contents = file_path.read_text(encoding="utf-8")
+            lines = contents.split(self.line_separator)
+            start = lines[0:start_line]
+            end = lines[end_line + 1 :]
+            return self.save_file(file_name=file_name, contents=self.line_separator.join(start + [chunk] + end))
+        except Exception as e:
+            log_error(f"Error patching file: {e}")
+            return f"Error patching file: {e}"
+
     def read_file(self, file_name: str) -> str:
         """Reads the contents of the file `file_name` and returns the contents if successful.
 
@@ -61,21 +129,67 @@ class FileTools(Toolkit):
         """
         try:
             log_info(f"Reading file: {file_name}")
-            file_path = self.base_dir.joinpath(file_name)
+            safe, file_path = self.check_escape(file_name)
+            if not (safe):
+                log_error(f"Attempted to read file: {file_name}")
+                return "Error reading file"
             contents = file_path.read_text(encoding="utf-8")
+            if len(contents) > self.max_file_length:
+                return "Error reading file: file too long. Use read_file_chunk instead"
+            if len(contents.split(self.line_separator)) > self.max_file_lines:
+                return "Error reading file: file too long. Use read_file_chunk instead"
+
             return str(contents)
         except Exception as e:
             log_error(f"Error reading file: {e}")
             return f"Error reading file: {e}"
 
-    def list_files(self) -> str:
-        """Returns a list of files in the base directory
+    def delete_file(self, file_name: str) -> str:
+        """Deletes a file
+        :param file_name: Name of the file to delete
+
+        :return: Empty string, if operation succeeded, otherwise returns an error message
+        """
+        safe, path = self.check_escape(file_name)
+        try:
+            if safe:
+                if path.is_dir():
+                    path.rmdir()
+                    return ""
+                path.unlink()
+                return ""
+            else:
+                log_error(f"Attempt to delete file outside {self.base_dir}: {file_name}")
+                return "Incorrect file_name"
+        except Exception as e:
+            log_error(f"Error removing {file_name}: {e}")
+            return str(e)
+
+    def check_escape(self, relative_path: str) -> Tuple[bool, Path]:
+        d = self.base_dir.joinpath(Path(relative_path)).resolve()
+        if self.base_dir == d:
+            return True, d
+        try:
+            d.relative_to(self.base_dir)
+        except ValueError:
+            log_error("Attempted to escape base_dir")
+            return False, self.base_dir
+        return True, d
+
+    def list_files(self, **kwargs) -> str:
+        """Returns a list of files in directory
+        :param directory: (Optional) name of directory to list.
 
         :return: The contents of the file if successful, otherwise returns an error message.
         """
+        directory = kwargs.get("directory", ".")
         try:
-            log_info(f"Reading files in : {self.base_dir}")
-            return json.dumps([str(file_path) for file_path in self.base_dir.iterdir()], indent=4)
+            log_info(f"Reading files in : {self.base_dir}/{directory}")
+            safe, d = self.check_escape(directory)
+            if safe:
+                return json.dumps([str(file_path) for file_path in d.iterdir()], indent=4)
+            else:
+                return "{}"
         except Exception as e:
             log_error(f"Error reading files: {e}")
             return f"Error reading files: {e}"
@@ -92,15 +206,23 @@ class FileTools(Toolkit):
 
             log_debug(f"Searching files in {self.base_dir} with pattern {pattern}")
             matching_files = list(self.base_dir.glob(pattern))
+            result = None
+            if self.expose_base_directory:
+                file_paths = [str(file_path) for file_path in matching_files]
+                result = {
+                    "pattern": pattern,
+                    "matches_found": len(file_paths),
+                    "base_directory": str(self.base_dir),
+                    "files": file_paths,
+                }
+            else:
+                file_paths = [str(file_path.relative_to(self.base_dir)) for file_path in matching_files]
 
-            file_paths = [str(file_path) for file_path in matching_files]
-
-            result = {
-                "pattern": pattern,
-                "base_directory": str(self.base_dir),
-                "matches_found": len(file_paths),
-                "files": file_paths,
-            }
+                result = {
+                    "pattern": pattern,
+                    "matches_found": len(file_paths),
+                    "files": file_paths,
+                }
             log_debug(f"Found {len(file_paths)} files matching pattern {pattern}")
             return json.dumps(result, indent=2)
 
---
+++ libs/agno/tests/unit/tools/test_file.py
@@ -0,0 +1,96 @@
+"""Unit tests for GitHub tools."""
+
+import json
+import tempfile
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from agno.tools.file import FileTools
+
+
+def test_list_files():
+    """Test listing files."""
+    wd = "."
+    f = FileTools(base_dir=Path(wd))
+    response = f.list_files(directory="libs")
+    file_list = json.loads(response)
+    assert len(file_list) > 0
+    response = f.list_files()
+    file_list = json.loads(response)
+    assert len(file_list) > 0
+    response = f.list_files(directory="..")
+    file_list = json.loads(response)
+    assert len(file_list) == 0
+    wd = "libs"
+    f = FileTools(base_dir=Path(wd))
+    response = f.list_files(directory="agno")
+    file_list = json.loads(response)
+    assert len(file_list) > 0
+    response = f.list_files()
+    file_list = json.loads(response)
+    assert len(file_list) > 0
+    response = f.list_files(directory="..")
+    file_list = json.loads(response)
+    assert len(file_list) == 0
+
+
+def test_read_file():
+    """Test read file"""
+    f = FileTools(base_dir=Path("."))
+    res = f.read_file(file_name="libs/agno/tests/unit/tools/testdir/testfile")
+    assert res == "testvalue\n"
+    res = f.read_file(file_name="../bla")
+    assert res == "Error reading file"
+    fsmall = FileTools(base_dir=Path("."), max_file_length=4)
+    res = fsmall.read_file(file_name="libs/agno/tests/unit/tools/testdir/testfile")
+    assert res == "Error reading file: file too long. Use read_file_chunk instead"
+
+
+def test_save_and_delete_file():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        f = FileTools(base_dir=Path(tmpdirname), enable_delete_file=True)
+        res = f.save_file(contents="contents", file_name="file.txt")
+        assert res == "file.txt"
+        contents = f.read_file(file_name="file.txt")
+        assert contents == "contents"
+        result = f.delete_file(file_name="file.txt")
+        assert result == ""
+        contents = f.read_file(file_name="file.txt")
+        assert contents != "contents"
+
+
+def test_search_files():
+    """Test search files"""
+    with tempfile.TemporaryDirectory() as tempdirname:
+        f = FileTools(base_dir=Path(tempdirname))
+        f.save_file(contents="contents", file_name="file1.txt")
+        f.save_file(contents="contents", file_name="file2.txt")
+        search_res = f.search_files("*")
+        parsed_result = json.loads(search_res)
+        assert parsed_result["pattern"] == "*"
+        assert parsed_result["matches_found"] == 2
+        assert set(parsed_result["files"]) == {"file1.txt", "file2.txt"}
+
+
+def test_read_file_chunk():
+    """Test chunked file read"""
+    with tempfile.TemporaryDirectory() as tempdirname:
+        f = FileTools(base_dir=Path(tempdirname))
+        f.save_file(contents="line0\nline1\nline2\nline3\n", file_name="file1.txt")
+        res = f.read_file_chunk(file_name="file1.txt", start_line=0, end_line=2)
+        assert res == "line0\nline1\nline2"
+        res = f.read_file_chunk(file_name="file1.txt", start_line=2, end_line=4)
+        assert res == "line2\nline3\n"
+
+
+def test_replace_file_chunk():
+    """Test replace file chunk"""
+    with tempfile.TemporaryDirectory() as tempdirname:
+        f = FileTools(base_dir=Path(tempdirname))
+        f.save_file(contents="line0\nline1\nline2\nline3\n", file_name="file1.txt")
+        res = f.replace_file_chunk(file_name="file1.txt", start_line=1, end_line=2, chunk="some\nstuff")
+        assert res == "file1.txt"
+        new_contents = f.read_file(file_name="file1.txt")
+        assert new_contents == "line0\nsome\nstuff\nline3\n"
