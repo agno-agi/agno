0c8017f831977dc9d1baec3f9c2cc77b5ab2ba84
anuragts <anuragsharma011011@gmail.com>
2025-10-15 19:17:04 +0530

    fix: update

--- libs/agno/agno/os/utils.py
+++ libs/agno/agno/os/utils.py
@@ -163,7 +163,7 @@ def extract_input_media(run_dict: Dict[str, Any]) -> Dict[str, Any]:
     input = run_dict.get("input", [])
     input_media["images"].extend(input.get("images", []))
     input_media["videos"].extend(input.get("videos", []))
-    input_media["audio"].extend(input.get("audios", []))
+    input_media["audios"].extend(input.get("audios", []))
     input_media["files"].extend(input.get("files", []))
     
     return input_media
