a63357792462d734eadccf25f90f92dc51036028
Mustafa-Esoofally <coolmusta@gmail.com>
2025-10-30 12:10:55 -0400

    update cookbook

--- cookbook/examples/teams/multi_purpose_team.py
+++ cookbook/examples/teams/multi_purpose_team.py
@@ -1,6 +1,5 @@
 from agno.agent import Agent
 from agno.models.anthropic import Claude
-from agno.models.deepseek import DeepSeek
 from agno.models.google.gemini import Gemini
 from agno.models.openai import OpenAIChat
 from agno.team.team import Team
