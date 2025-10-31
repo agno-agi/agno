5a8a71781dd2d8055109d09256c5c4d6ad6349f4
ysolanky <yash@phidata.com>
2025-10-31 17:51:08 -0400

    update

--- libs/agno/agno/workflow/workflow.py
+++ libs/agno/agno/workflow/workflow.py
@@ -972,13 +972,13 @@ class Workflow:
         event: "WorkflowRunOutputEvent",
         workflow_run_response: WorkflowRunOutput,
         websocket_handler: Optional[WebSocketHandler] = None,
-    ) -> Optional["WorkflowRunOutputEvent"]:
+    ) -> "WorkflowRunOutputEvent":
         """Handle workflow events for storage - similar to Team._handle_event"""
         from agno.run.agent import RunOutput
         from agno.run.team import TeamRunOutput
 
         if isinstance(event, RunOutput) or isinstance(event, TeamRunOutput):
-            return None
+            return event
         if self.store_events:
             # Check if this event type should be skipped
             if self.events_to_skip:
