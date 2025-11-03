5aa7326f0003902982d2fab03738dd92280d31af
kausmeows <shuklakaustubh84@gmail.com>
2025-11-03 16:48:00 +0530

    update

--- cookbook/agent_os/tracing/basic_agent_tracing.py
+++ cookbook/agent_os/tracing/basic_agent_tracing.py
@@ -34,4 +34,4 @@ agent_os = AgentOS(
 app = agent_os.get_app()
 
 if __name__ == "__main__":
-    agent_os.serve(app="test:app", reload=True)
+    agent_os.serve(app="basic_agent_tracing:app", reload=True)
