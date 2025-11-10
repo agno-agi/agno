78ec53b28c0980e96841f4aa8e8be275cc1b12a0
manu <manuhortet@gmail.com>
2025-11-10 16:26:04 +0100

    Update cookbook/agent_os/interfaces/slack/basic.py

--- cookbook/agent_os/interfaces/slack/basic.py
+++ cookbook/agent_os/interfaces/slack/basic.py
@@ -20,8 +20,9 @@ agent_os = AgentOS(
     agents=[basic_agent],
     interfaces=[
         Slack(
-            agent=basic_agent, reply_to_mentions_only=True
-        )  # bot replies only when mentioned
+            agent=basic_agent, 
+            reply_to_mentions_only=True, # The Agent will react only to messages mentioning it
+        ) 
     ],
 )
 app = agent_os.get_app()
