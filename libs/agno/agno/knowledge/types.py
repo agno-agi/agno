d321288c03c4bc67043d7aa6b26bbedc6499d69b
Willem Carel de Jongh <willemcarel@gmail.com>
2025-11-12 08:08:51 +0200

    updates

--- libs/agno/agno/knowledge/types.py
+++ libs/agno/agno/knowledge/types.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Union
+from typing import Any
 
 from pydantic import BaseModel
 
@@ -36,4 +36,4 @@ def get_content_type_enum(content_type_str: str) -> ContentType:
 
 class KnowledgeFilter(BaseModel):
     key: str
-    value: Union[str, int, float, bool]
+    value: Any
