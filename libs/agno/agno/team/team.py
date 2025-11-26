600bd7ab117fa8096df3b10dbe0d8a3f98dc2c83
ysolanky <yash@phidata.com>
2025-11-26 15:00:22 -0500

    feat: agno debug level env variable

--- libs/agno/agno/team/team.py
+++ libs/agno/agno/team/team.py
@@ -748,8 +748,10 @@ class Team:
             self.id = generate_id_from_name(self.name)
 
     def _set_debug(self, debug_mode: Optional[bool] = None) -> None:
+        debug_level = int(env) if (env := getenv("AGNO_DEBUG_LEVEL")) in ("1", "2") else self.debug_level
+
         if self.debug_mode or debug_mode or getenv("AGNO_DEBUG", "false").lower() == "true":
-            set_log_level_to_debug(source_type="team", level=self.debug_level)
+            set_log_level_to_debug(source_type="team", level=debug_level)
         else:
             set_log_level_to_info(source_type="team")
 
