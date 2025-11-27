5938b20c8036bdededa2c49822708789bc4db85d
thegeosman <131323655+thegeosman@users.noreply.github.com>
2025-11-26 23:03:20 -0300

    Update duckduckgo.py
    Fixing the error with duckduckgo tool, that was originated in the changes in ddgs package backends names from lower case to first letter upper case as per bellow 
    
    """
            fix for the errors, the ddgs package changed the names of backend engines from lowercase to upper case
                "text": {
                "bing": Bing,
                "brave": Brave,
                "duckduckgo": Duckduckgo,  # bing
                "google": Google,
                "mojeek": Mojeek,
                "mullvad_brave": MullvadLetaBrave,  # brave
                "mullvad_google": MullvadLetaGoogle,  # google
                "yahoo": Yahoo,  # bing
                "yandex": Yandex,
                "wikipedia": Wikipedia,
            },
            "images": {
                "duckduckgo": DuckduckgoImages,
            },
            "news": {
                "duckduckgo": DuckduckgoNews,
            },
            "videos": {
                "duckduckgo": DuckduckgoVideos,
            },
        """

--- libs/agno/agno/tools/duckduckgo.py
+++ libs/agno/agno/tools/duckduckgo.py
@@ -11,6 +11,7 @@ except ImportError:
 
 
 class DuckDuckGoTools(Toolkit):
+
     """
     DuckDuckGo is a toolkit for searching using DuckDuckGo easily.
     It uses the meta-search library DDGS, so it also has access to other backends.
@@ -22,7 +23,31 @@ class DuckDuckGoTools(Toolkit):
         proxy (Optional[str]): Proxy to be used in the search request.
         timeout (Optional[int]): The maximum number of seconds to wait for a response.
         backend (Optional[str]): The backend to be used in the search request.
+    """
 
+    """
+        fix for the errors, the ddgs package changed the names of backend engines from lowercase to upper case
+            "text": {
+            "bing": Bing,
+            "brave": Brave,
+            "duckduckgo": Duckduckgo,  # bing
+            "google": Google,
+            "mojeek": Mojeek,
+            "mullvad_brave": MullvadLetaBrave,  # brave
+            "mullvad_google": MullvadLetaGoogle,  # google
+            "yahoo": Yahoo,  # bing
+            "yandex": Yandex,
+            "wikipedia": Wikipedia,
+        },
+        "images": {
+            "duckduckgo": DuckduckgoImages,
+        },
+        "news": {
+            "duckduckgo": DuckduckgoNews,
+        },
+        "videos": {
+            "duckduckgo": DuckduckgoVideos,
+        },
     """
 
     def __init__(
@@ -30,9 +55,11 @@ class DuckDuckGoTools(Toolkit):
         enable_search: bool = True,
         enable_news: bool = True,
         all: bool = False,
-        backend: str = "duckduckgo",
+        backend: str = "Duckduckgo",
         modifier: Optional[str] = None,
         fixed_max_results: Optional[int] = None,
+        time_limit: str = "y",
+        region: str = "us-en",
         proxy: Optional[str] = None,
         timeout: Optional[int] = 10,
         verify_ssl: bool = True,
@@ -44,6 +71,8 @@ class DuckDuckGoTools(Toolkit):
         self.modifier: Optional[str] = modifier
         self.verify_ssl: bool = verify_ssl
         self.backend: str = backend
+        self.time_limit: Optional[str] = time_limit
+        self.region: Optional[str] =  region
 
         tools: List[Any] = []
         if all or enable_search:
@@ -51,9 +80,9 @@ class DuckDuckGoTools(Toolkit):
         if all or enable_news:
             tools.append(self.duckduckgo_news)
 
-        super().__init__(name="duckduckgo", tools=tools, **kwargs)
+        super().__init__(name="Duckduckgo", tools=tools, **kwargs)
 
-    def duckduckgo_search(self, query: str, max_results: int = 5) -> str:
+    def duckduckgo_search(self, query: str, max_results: int = 5, time_limit: str = "y", region: str = "us-en") -> str:
         """Use this function to search DDGS for a query.
 
         Args:
@@ -65,14 +94,16 @@ class DuckDuckGoTools(Toolkit):
         """
         actual_max_results = self.fixed_max_results or max_results
         search_query = f"{self.modifier} {query}" if self.modifier else query
+        time_limit = self.time_limit or time_limit
+        region = self.region or region
 
         log_debug(f"Searching DDG for: {search_query} using backend: {self.backend}")
         with DDGS(proxy=self.proxy, timeout=self.timeout, verify=self.verify_ssl) as ddgs:
-            results = ddgs.text(query=search_query, max_results=actual_max_results, backend=self.backend)
+            results = ddgs.text(query=search_query, max_results=actual_max_results, backend=self.backend, timelimit=time_limit, region=region)
 
         return json.dumps(results, indent=2)
 
-    def duckduckgo_news(self, query: str, max_results: int = 5) -> str:
+    def duckduckgo_news(self, query: str, max_results: int = 5, time_limit: str = "y", region: str = "us-en") -> str:
         """Use this function to get the latest news from DDGS.
 
         Args:
@@ -83,9 +114,10 @@ class DuckDuckGoTools(Toolkit):
             The latest news from DDGS.
         """
         actual_max_results = self.fixed_max_results or max_results
-
+        time_limit = self.time_limit or time_limit
+        region = self.region or region
         log_debug(f"Searching DDG news for: {query} using backend: {self.backend}")
         with DDGS(proxy=self.proxy, timeout=self.timeout, verify=self.verify_ssl) as ddgs:
-            results = ddgs.news(query=query, max_results=actual_max_results, backend=self.backend)
+            results = ddgs.news(query=query, max_results=actual_max_results, backend=self.backend, timelimit=time_limit, region=region)
 
         return json.dumps(results, indent=2)
