44a52cde6132948a6df8eecb9fb201fbe0bd9cb6
Willem Carel de Jongh <willemcarel@gmail.com>
2026-01-19 13:14:57 +0200

    fix: Update failing integration test prompt

--- libs/agno/tests/integration/agent/test_custom_retriever.py
+++ libs/agno/tests/integration/agent/test_custom_retriever.py
@@ -29,7 +29,6 @@ def test_agent_with_custom_knowledge_retriever_error():
     assert response.metadata is None, "There should be no references"
     assert "<references>" not in response.messages[0].content  # type: ignore
 
-
 def test_agent_with_custom_knowledge_retriever_search_knowledge_error():
     def custom_knowledge_retriever(**kwargs):
         raise Exception("Test error")
@@ -39,8 +38,10 @@ def test_agent_with_custom_knowledge_retriever_search_knowledge_error():
         knowledge_retriever=custom_knowledge_retriever,
         search_knowledge=True,
         debug_mode=True,
+        instructions="Always search the knowledge base for information before answering."
     )
     response = agent.run("Search my knowledge base for information about the capital of France")
     assert response.metadata is None, "There should be no references"
     assert response.tools and response.tools[0].tool_name == "search_knowledge_base"
     assert response.content is not None
+
