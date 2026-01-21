76248feccad71b6e71c492bb06545f896290f654
Kaustubh <shuklakaustubh84@gmail.com>
2026-01-21 16:36:01 +0530

    Update cookbook/90_models/n1n/basic.py

--- cookbook/90_models/n1n/basic.py
+++ cookbook/90_models/n1n/basic.py
@@ -3,5 +3,4 @@ from agno.models.n1n import N1N
 
 agent = Agent(model=N1N(id="gpt-4o"), markdown=True)
 
-# Print the response in the terminal
 agent.print_response("Share a 2 sentence horror story.")
