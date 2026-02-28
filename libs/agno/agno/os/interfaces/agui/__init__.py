848658a64227db6c2349d1ca9ea649ffef13ae56
Br1an67 <932039080@qq.com>
2026-02-28 19:02:05 +0800

    fix: resolve Pydantic v2 field alias warnings in AGUI models

--- libs/agno/agno/os/interfaces/agui/__init__.py
+++ libs/agno/agno/os/interfaces/agui/__init__.py
@@ -1,3 +1,16 @@
+import warnings
+
 from agno.os.interfaces.agui.agui import AGUI
+
+# Suppress Pydantic v2 UnsupportedFieldAttributeWarning from ag_ui models.
+# The ag_ui library uses alias_generator=to_camel in ConfigDict, which in some
+# Pydantic v2 versions triggers spurious warnings about field aliases having no
+# effect when models are used in discriminated unions or FastAPI route parameters.
+warnings.filterwarnings(
+    "ignore",
+    message=r".*alias.*",
+    category=UserWarning,
+    module=r"pydantic\._internal\._generate_schema",
+)
 
 __all__ = ["AGUI"]
