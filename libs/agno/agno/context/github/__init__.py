18a234997fa43a60adefed79ace26ff139014970
Mustafa Esoofally <coolmusta@gmail.com>
2026-04-27 13:17:50 -0400

    refactor(context): remove internal exports from github __init__

--- libs/agno/agno/context/github/__init__.py
+++ libs/agno/agno/context/github/__init__.py
@@ -3,13 +3,9 @@ from agno.context.github.provider import (
     DEFAULT_GITHUB_WRITE_INSTRUCTIONS,
     GitHubContextProvider,
 )
-from agno.context.github.tools import DEFAULT_MAX_OUTPUT_CHARS, GitReadTools, GitWriteTools
 
 __all__ = [
     "DEFAULT_GITHUB_READ_INSTRUCTIONS",
     "DEFAULT_GITHUB_WRITE_INSTRUCTIONS",
-    "DEFAULT_MAX_OUTPUT_CHARS",
     "GitHubContextProvider",
-    "GitReadTools",
-    "GitWriteTools",
 ]
