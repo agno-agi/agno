b78a75c8b8c17fdd0d728324bda81b401f9654a0
jupiter <1404785625@qq.com>
2026-07-06 16:33:11 +0800

    [fix] exclude custom events from tool results

--- libs/agno/agno/models/base.py
+++ libs/agno/agno/models/base.py
@@ -2199,7 +2199,6 @@ class Model(ABC):
                                 yield ModelResponse(content=item.content)
 
                         if isinstance(item, CustomEvent):
-                            function_call_output += str(item)
                             item.tool_call_id = function_call.call_id
 
                         # For WorkflowCompletedEvent, extract content for final output
@@ -2736,7 +2735,6 @@ class Model(ABC):
                                 continue
 
                         if isinstance(item, CustomEvent):
-                            function_call_output += str(item)
                             item.tool_call_id = function_call.call_id
 
                             # For WorkflowCompletedEvent, extract content for final output
@@ -2874,7 +2872,6 @@ class Model(ABC):
                                     continue
 
                             elif isinstance(item, CustomEvent):
-                                function_call_output += str(item)
                                 item.tool_call_id = function_call.call_id
 
                             # Yield the event itself to bubble it up
