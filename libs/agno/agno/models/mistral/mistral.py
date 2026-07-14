184d7e75cbe02bd5569f35e7fea78cb1a2fa82ab
Harsh Sinha <sinha03harsh@gmail.com>
2026-07-14 19:52:45 +0530

    update

--- libs/agno/agno/models/mistral/mistral.py
+++ libs/agno/agno/models/mistral/mistral.py
@@ -53,7 +53,6 @@ class MistralChat(Model):
     frequency_penalty: Optional[float] = None
     presence_penalty: Optional[float] = None
     stop: Optional[Union[str, List[str]]] = None
-    n: Optional[int] = None
     safe_mode: bool = False
     safe_prompt: bool = False
     request_params: Optional[Dict[str, Any]] = None
@@ -118,22 +117,20 @@ class MistralChat(Model):
             Dict[str, Any]: The API kwargs.
         """
         _request_params: Dict[str, Any] = {}
-        if self.temperature:
+        if self.temperature is not None:
             _request_params["temperature"] = self.temperature
-        if self.max_tokens:
+        if self.max_tokens is not None:
             _request_params["max_tokens"] = self.max_tokens
-        if self.top_p:
+        if self.top_p is not None:
             _request_params["top_p"] = self.top_p
-        if self.random_seed:
+        if self.random_seed is not None:
             _request_params["random_seed"] = self.random_seed
-        if self.frequency_penalty:
+        if self.frequency_penalty is not None:
             _request_params["frequency_penalty"] = self.frequency_penalty
-        if self.presence_penalty:
+        if self.presence_penalty is not None:
             _request_params["presence_penalty"] = self.presence_penalty
-        if self.stop:
+        if self.stop is not None:
             _request_params["stop"] = self.stop
-        if self.n:
-            _request_params["n"] = self.n
         if self.safe_mode:
             _request_params["safe_mode"] = self.safe_mode
         if self.safe_prompt:
@@ -163,11 +160,11 @@ class MistralChat(Model):
             {
                 "temperature": self.temperature,
                 "max_tokens": self.max_tokens,
+                "top_p": self.top_p,
                 "random_seed": self.random_seed,
                 "frequency_penalty": self.frequency_penalty,
                 "presence_penalty": self.presence_penalty,
                 "stop": self.stop,
-                "n": self.n,
                 "safe_mode": self.safe_mode,
                 "safe_prompt": self.safe_prompt,
             }
