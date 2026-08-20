fdfd209ebcebf3e2a27ba2b607eb1d7c627b0dfe
Mustafa Esoofally <coolmusta@gmail.com>
2026-08-20 19:15:40 -0400

    fix: revert agentql parameter name to custom_scrape_website
    Method stays as agentql_custom_scrape_website (for LLM tool disambiguation),
    but parameter reverts to custom_scrape_website (params are toolkit-local,
    no conflict possible, consistent with other toolkits).

--- libs/agno/agno/tools/agentql.py
+++ libs/agno/agno/tools/agentql.py
@@ -17,15 +17,15 @@ class AgentQLTools(Toolkit):
         api_key: Optional[str] = None,
         agentql_query: str = "",
         scrape_website: bool = True,
-        agentql_custom_scrape_website: bool = False,
+        custom_scrape_website: bool = False,
         all: bool = False,
         **kwargs,
     ):
         # Backwards compat: enable_X -> X
         if "enable_scrape_website" in kwargs:
             scrape_website = kwargs.pop("enable_scrape_website")
-        if "enable_agentql_custom_scrape_website" in kwargs:
-            agentql_custom_scrape_website = kwargs.pop("enable_agentql_custom_scrape_website")
+        if "enable_custom_scrape_website" in kwargs:
+            custom_scrape_website = kwargs.pop("enable_custom_scrape_website")
 
         self.api_key = api_key or getenv("AGENTQL_API_KEY")
         if not self.api_key:
@@ -36,7 +36,7 @@ class AgentQLTools(Toolkit):
         tools: List[Callable] = []
         if all or scrape_website:
             tools.append(self.agentql_scrape_website)
-        if all or agentql_custom_scrape_website or agentql_query:
+        if all or custom_scrape_website or agentql_query:
             tools.append(self.agentql_custom_scrape_website)
 
         super().__init__(name="agentql_tools", tools=tools, **kwargs)
