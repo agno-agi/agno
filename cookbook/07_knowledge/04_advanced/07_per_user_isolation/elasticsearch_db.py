8af402b3063220c24e329d11d004137b5d6082d1
Harsh Sinha <sinha03harsh@gmail.com>
2026-09-08 19:28:52 +0530

    Update

--- cookbook/07_knowledge/04_advanced/07_per_user_isolation/elasticsearch_db.py
+++ cookbook/07_knowledge/04_advanced/07_per_user_isolation/elasticsearch_db.py
@@ -16,7 +16,9 @@ all belong to another owner.
 - Search with user_id=None: admin view, sees everything
 
 Requirements:
+- ./cookbook/scripts/run_elasticsearch.sh
 - uv pip install elasticsearch
+- OPENAI_API_KEY
 """
 
 import asyncio
