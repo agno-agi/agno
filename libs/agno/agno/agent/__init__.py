8493371ce0d91d2b1920c0a9ff532101751b1244
Ashpreet <bediashpreet@gmail.com>
2026-09-09 20:12:21 +0100

    style: sort follow-up configuration export

--- libs/agno/agno/agent/__init__.py
+++ libs/agno/agno/agent/__init__.py
@@ -7,8 +7,8 @@ from agno.agent.agent import (
     get_agent_by_id,
     get_agents,
 )
-from agno.agent.followup import FollowupConfig
 from agno.agent.factory import AgentFactory
+from agno.agent.followup import FollowupConfig
 from agno.agent.remote import RemoteAgent
 from agno.factory import (
     BaseFactory,
